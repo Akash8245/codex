89a76da0b4153cd13d2b9d0cbd62e4b97218a273
Akash <akash93433@gmail.com>
2024-03-11 21:39:37 +0530

    Minor Changes

--- src/components/DS.jsx
+++ src/components/DS.jsx
@@ -15,10 +15,10 @@ const DS = () => {
   };
 
   return (
-    <div className='bg-[#3a3939] h-[100%] text-white p-5'>
+    <div className='bg-[#3a3939]  text-white p-3'>
       <div className='flex'>
         <div
-          className={` cursor-pointer p-4 ${
+          className={` cursor-pointer p-3 h-[80px] md:h-[50px] text-[12px] md:text-[18px] text-center ${
             activeTab === 1 ? 'bg-blue-800 text-white' : 'bg-gray-600'
           }`}
           onClick={() => handleTabClick(1)}
@@ -26,7 +26,7 @@ const DS = () => {
           Lab 1
         </div>
         <div
-          className={`cursor-pointer p-4 ${
+          className={`cursor-pointer p-3 h-[80px] md:h-[50px] text-[12px] md:text-[18px] text-center ${
             activeTab === 2 ? 'bg-blue-800 text-white' : 'bg-gray-600'
           }`}
           onClick={() => handleTabClick(2)}
@@ -34,7 +34,7 @@ const DS = () => {
           Lab 2
         </div>
         <div
-          className={`cursor-pointer p-4 ${
+          className={`cursor-pointer p-3 h-[80px] md:h-[50px] text-[12px] md:text-[18px] text-center ${
             activeTab === 3 ? 'bg-blue-800 text-white' : 'bg-gray-600'
           }`}
           onClick={() => handleTabClick(3)}
@@ -42,7 +42,7 @@ const DS = () => {
           Lab 3
         </div>
         <div
-          className={`cursor-pointer p-4 ${
+          className={`cursor-pointer p-3 h-[80px] md:h-[50px] text-[12px] md:text-[18px] text-center ${
             activeTab === 4 ? 'bg-blue-800 text-white' : 'bg-gray-600'
           }`}
           onClick={() => handleTabClick(4)}
@@ -50,7 +50,7 @@ const DS = () => {
           Lab 4
         </div>
         <div
-          className={`cursor-pointer p-4 ${
+          className={`cursor-pointer p-3 h-[80px] md:h-[50px] text-[12px] md:text-[18px] text-center ${
             activeTab === 5 ? 'bg-blue-800 text-white' : 'bg-gray-600'
           }`}
           onClick={() => handleTabClick(5)}
@@ -58,7 +58,7 @@ const DS = () => {
           Lab 5
         </div>
         <div
-          className={`cursor-pointer p-4 ${
+          className={`cursor-pointer p-3 h-[80px] md:h-[50px] text-[12px] md:text-[18px] text-center ${
             activeTab === 6 ? 'bg-blue-800 text-white' : 'bg-gray-600'
           }`}
           onClick={() => handleTabClick(6)}
@@ -66,14 +66,14 @@ const DS = () => {
           Lab 6
         </div>
         <div
-          className={`cursor-pointer p-4 ${
+          className={`cursor-pointer p-3 h-[80px] md:h-[50px] text-[12px] md:text-[18px] text-center ${
             activeTab === 7 ? 'bg-blue-800 text-white' : 'bg-gray-600'
           }`}
           onClick={() => handleTabClick(7)}
         >
           Lab 7
         </div>
-          <h1 className='pt-4 pl-5 font-bold'>Happy Copy Pasting ! (˶ᵔ ᵕ ᵔ˶)</h1>
+
       </div>
       <div className='mt-4 p-4 '>
         {activeTab === 1 && <p><Lab1/></p>}
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Nav() {
   return (
     <div>
-      <nav className='flex justify-between pl-12 h-[60px] bg-black text-white text-[30px] space-x-5 pt-2 '>
+      <nav className='flex justify-between pl-12 h-[60px] bg-black text-white text-[20px] md:text-[30px] space-x-5 pt-3.5 md:pt-2 '>
         <div className='cursor-pointer font-bold '>
           <p>Code<span className='text-slate-200'>X</span></p>
         </div>
