396c1b65730ed586cd6005e9672607f86c722f95
Akash <142102579+Akash8245@users.noreply.github.com>
2024-03-14 20:20:29 +0530

    Update Lab5.jsx

--- src/java_contents/Lab5.jsx
+++ src/java_contents/Lab5.jsx
@@ -1,15 +1,79 @@
 import React from 'react';
 import CodeViewer from '../components/CodeViewer';
 
-const code = `Dont have the code if you have send it i will update :( `;
+const code = `package test;
+
+class one extends Thread {
+    public void run() {
+        for(int i = 0; i < 3; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch(InterruptedException e) {
+                System.out.print(e);
+            }
+            System.out.print("\nGood Morning");
+        }
+    }
+}
+
+class two extends Thread {
+    public void run() {
+        for(int i = 0; i < 3; i++) {
+            try {
+                Thread.sleep(2000);
+            } catch(InterruptedException e) {
+                System.out.print(e);
+            }
+            System.out.print("\nHello");
+        }
+    }
+}
+
+class three implements Runnable {
+    public void run() {
+        for(int i = 0; i < 3; i++) {
+            try {
+                Thread.sleep(2000);
+            } catch(InterruptedException e) {
+                System.out.print(e);
+            }
+            System.out.print("\nWelcome !");
+        }
+    }
+}
+
+public class lab5 {
+    public static void main(String[] args) {
+        one t1 = new one();
+        two t2 = new two();
+        Thread t3 = new Thread(new three());
+        
+        t1.setName("Thread one");
+        t2.setName("Thread two");
+        t3.setName("Thread three");
+        
+        System.out.print(t1.getName());
+        System.out.print(t2.getName());
+        System.out.print(t3.getName());
+        
+        Thread mainThread = Thread.currentThread();
+        System.out.print(mainThread);
+        
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
+`;
 
 const Lab5 = () => {
   return (
     <div>
-      <h1 className='pb-2'>Comming Soon !</h1>
+      <h1 className='pb-2'>Java program to implement Multi Threading</h1>
       <CodeViewer code={code} />
     </div>
   );
 };
 
 export default Lab5;
+
