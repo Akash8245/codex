e2235d54bb6ce444e21a578f7e4db54a683ccfe0
Akash <142102579+Akash8245@users.noreply.github.com>
2024-03-14 20:20:05 +0530

    Update Lab4.jsx

--- src/java_contents/Lab4.jsx
+++ src/java_contents/Lab4.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import CodeViewer from '../components/CodeViewer';
 
-const code = `Dont have the code if you have send it i will update :(`;
+const code = `package test;
+import java.lang.Math;
+public class lab4 {
+
+	public static void main(String[] args) {
+		overload_demo ob = new overload_demo();
+		ob.area(5);
+		ob.area(11,12);
+		ob.area(3,4,5);
+
+	}
+
+}
+class overload_demo{
+	void area(float x)
+	{
+		System.out.print("\nThe area of Square is "+Math.pow(x,2));
+	}
+	void area(float x,float y) {
+		System.out.print("\nThe area od rectangle "+x*y);
+	}
+	void area(float x,float y,float z) {
+		double s=(x+y+z)/2;
+		double a = Math.sqrt((s-x)*(s-y)*(s-z)*s);
+		System.out.print("\nThe area of triangle "+a);
+	}
+}`;
 
 const Lab4 = () => {
   return (
     <div>
-      <h1 className='pb-2'>Comming Soon !</h1>
+      <h1 className='pb-2'>Program to display areas of Rectangle , Square and Triangle</h1>
       <CodeViewer code={code} />
     </div>
   );
