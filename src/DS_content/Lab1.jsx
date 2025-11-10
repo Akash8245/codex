ef0ff7a9940f12b25c1edccfa1d77033af010579
Akash <142102579+Akash8245@users.noreply.github.com>
2025-11-10 10:05:16 +0530

    Update Lab1.jsx

--- src/DS_content/Lab1.jsx
+++ src/DS_content/Lab1.jsx
@@ -2,28 +2,30 @@
 import React from 'react';
 import CodeViewer from '../components/CodeViewer';
 
-const code = `#include <stdio.h>
-#include <conio.h>
+const code = `
+  import numpy as np import pandas as pd
 
-int lengthstring(char str[20], int i) {
-  if (str[i] == '0')
-    return (0);
-  else
-    return (1 + lengthstring(str, i + 1));
-}
+arr np.array([[10, 20, 30], [40, 50, 60]])
 
-void main() {
-  int length;
-  char str[20];
-  clrscr();
-  printf("Program to calculate length of the string using recursion");
-  printf("----------------------------------------------------------");
-  printf("Enter a string ");
-  gets(str);
-  length = lengthstring(str, 0);
-  printf("String is %s and its length is %d", str, length);
-  getch();
-}`;
+print("Array: \n", arr)
+
+print("Sum:", np.sum(arr), "Mean:", np.mean(arr), "Max:", np.max(arr))
+
+data
+
+("Name": ["John', 'Alice', 'Bob', 'David', 'Eve"],
+
+'Age': [25, np.nan, 30, 22, 28],
+
+'Salary': [50000, 60000, np.nan, 52000, 58008), 'Dept': ['HR', 'IT', 'Finance', 'IT', 'HR'])
+
+df= pd.DataFrame(data) print("\nOriginal Data:\n", df)
+
+df['Age'] df['Age"].fillna(df['Age'].mean()) df[ 'Salary] df['Salary'].fillna(df['Salary'].mean()) print("\nCleaned Data:\n", df)
+
+print("\nFiltered (Age>25):\n", df [df['Age'] 25]) print("\nMean Salary by Dept:\n", df.groupby('Dept')['Salary'].mean())
+
+`;
 
 const Lab1 = () => {
   return (
